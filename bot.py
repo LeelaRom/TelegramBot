f8930232791ade7bfef910de8110844568d16f8f
ERomanova <leelarom@gmail.com>
2018-04-24 17:14:41 +0300

    Added 2 functions

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 '''
-Created on 23 ���. 2018 �.
+Created on 23 apr. 2018 �.
 
 @author: leela
 '''
-import sys
 
-# -*- coding: utf-8 -*-
 import config
 import telebot
 
@@ -16,5 +14,17 @@ bot = telebot.TeleBot(config.token)
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
+  
+#Обработчик команды start    
+@bot.message_handler(commands = ["start"])
+def start(message):
+    sent = bot.send_message(message.chat.id, "What is your name?")
+    bot.register_next_step_handler(sent, hello)
+    
+    
+def hello(message):
+    bot.send_message(message.chat.id, "Hello, {name}, I am happy to see you.".format(mane=message.text))
+    
+    
 if __name__ == '__main__':
     bot.polling(none_stop=True)
